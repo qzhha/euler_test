f89f63c22f60d5a706bc65e6cf434ea25c12bdff
qzhha <zivqin665@gmail.com>
2021-12-29 21:59:45 +0800

    claystack update

--- claystack/invite.js
+++ claystack/invite.js
@@ -2,20 +2,53 @@ const ethers = require('ethers');
 require("dotenv").config();
 const provider = new ethers.providers.JsonRpcProvider("https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161");
 const axios = require('axios');
-const abi = require('../abi');
+const abicoder = new ethers.utils.AbiCoder()
+const {complete_tx,load_accounts_excel} = require('../utils')
 
-//let keys = process.env.KEYS.split(",");
+let keys = process.env.KEYS.split(",");
 
-async function start()
+function get_invite_data(address)
 {
-    const abi_req = await axios.get('http://api-goerli.etherscan.io/api?module=contract&action=getabi&address=0x1b8CCBf5E8dBE1599905349E24b92a61175F9A10&format=raw')
-    if (abi_req.status != 200)
-    {
-        console.log("get abi err.%s",abi_req.status)
-        return
-    }
-
-    console.log(abi_req)
+    return '0x65b8e524' + abicoder.encode(["address"],[address]).slice(2)
 }
 
-start()
+async function invite(wallet,address)
+{
+    var tx_data = get_invite_data(address)
+    await complete_tx('invite '+address,wallet.sendTransaction({to:'0xf1869edab2c3ab3461f2a2bc521a016c75e90553',data:tx_data,gasLimit:500000}),3)
+}
+async function startloop()
+{
+    // 无脑邀请x+1个人
+    
+    var wallets = load_accounts_excel('../new_matcha.xls');
+    
+    var cur_idx = 0
+
+    while (cur_idx < length)
+    {
+        if (cur_idx + 1 < length)
+        {
+            await invite(wallets[cur_idx],wallets[cur_idx+1].address)
+        }
+        else if (cur_idx+2 < length)
+        {
+            await invite(wallets[cur_idx],wallets[cur_idx+2].address)
+        }
+        else if (cur_idx + 3 < length)
+        {
+            await invite(wallets[cur_idx],wallets[cur_idx+3].address)
+        }
+        else if (cur_idx + 4 < length)
+        {
+            await invite(cur_idx[cur_idx],cur_idx[cur_idx+4].address)
+        }
+        else if (cur_idx + 5 < length)
+        {
+            await invite(wallets[cur_idx],wallets[cur_idx+5].address)
+        }
+        cur_idx = cur_idx + 1;
+    }
+}
+
+startloop()
